7cc7c70b9b8fb5189d51d6b4f361e456d0c461e5
Prathmesh Bhatt <ppb190002@utdallas.edu>
2022-11-13 09:02:23 -0600

    Page2Progression

--- src/Analytics/Graph.js
+++ src/Analytics/Graph.js
@@ -4,43 +4,43 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 const data = [
   {
-    name: 'Page A',
+    name: 'Asteroid 1',
     uv: 1000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: 'Asteroid 2',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: 'Asteroid 3',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: 'Asteroid 4',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: 'Asteroid 5',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: 'Asteroid 6',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: 'Asteroid 7',
     uv: 3490,
     pv: 4300,
     amt: 2100,
